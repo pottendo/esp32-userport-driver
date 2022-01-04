25d9e72e2a33e33032a53f1d25eed9dbc010354a
pottendo <62503913+pottendo@users.noreply.github.com>
2022-01-04 11:05:16 +0100

    Update mqtt-cred.h

--- src/mqtt-cred.h
+++ src/mqtt-cred.h
@@ -1,13 +1,7 @@
-/* not to be stored publicly */
-#define MQTT_LOG_LOCAL
+//#define MQTT_LOG_LOCAL
 
-#ifdef MQTT_LOG_LOCAL
-#define MQTT_LOG "pottendo-pi30-phono"
-#else
-#define MQTT_LOG "node02.myqtthub.com"
-#endif
-
-#define MQTT_LOG_USER "XXXuser"
-#define MQTT_LOG_PW "XXXPW"
-
-#define MQTT_CRED
+/*
+#define MQTT_LOG ""
+#define MQTT_LOG_USER "XXX"
+#define MQTT_LOG_PW "XXX"
+*/
