a511eb61031a2c8b0e3e3fed04245a40de81c627
pottendo <pottendo@gmx.net>
2022-01-04 12:04:37 +0100

    Credits template

---
+++ src/cred.h
@@ -0,0 +1,16 @@
+#ifndef __CRED_H__
+#define __CRED_H__
+
+//#define IRC_CRED        // define this when the IRC macros below have been set!
+#define IRC_SERVER "irc.libera.chat"        // choos your IRC server and port accordingly
+#define IRC_PORT 6697
+#define IRC_NICKNAME ""
+#define IRC_USER ""
+#define IRC_CHANNEL "#..."  
+
+//#define MQTT_CRED       // define this when the IRC macros below have been set!
+#define MQTT_LOG "test.mosquitto.org"       // a public MQTT service, better use a local in your network
+#define MQTT_LOG_USER ""
+#define MQTT_LOG_PW ""
+
+#endif
