f6e740753d1b1132a2bc25869f917dc20bd566b1
pottendo <pottendo@gmx.net>
2022-01-04 16:10:23 +0100

    Update.

--- src/cred.h
+++ src/cred.h
@@ -1,15 +1,15 @@
 #ifndef __CRED_H__
 #define __CRED_H__
 
-//#define IRC_CRED        // define this when the IRC macros below have been set!
-#define IRC_SERVER "irc.libera.chat"        // choos your IRC server and port accordingly
+#define IRC_CRED        // define this when the IRC macros below have been set!
+#define IRC_SERVER "irc.libera.chat"
 #define IRC_PORT 6697
-#define IRC_NICKNAME ""
-#define IRC_USER ""
-#define IRC_CHANNEL "#..."  
+#define IRC_NICKNAME "pottendosC64"
+#define IRC_USER "pottendo"
+#define IRC_CHANNEL "#vice-dev"
 
-//#define MQTT_CRED       // define this when the IRC macros below have been set!
-#define MQTT_LOG "test.mosquitto.org"       // a public MQTT service, better use a local in your network
+#define MQTT_CRED       // define this when the IRC macros below have been set!
+#define MQTT_LOG "pottendo-pi30-phono"
 #define MQTT_LOG_USER ""
 #define MQTT_LOG_PW ""
 
